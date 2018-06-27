4b82d292ef56ed83e4fd71f7ab4b7e9e63bef774
Jullachai <onimaruz@hotmail.com>
2018-06-27 08:11:03 +0700

    no message

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import FCM, { FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType } from 'react-native-fcm';
 
 export default class App extends React.Component {
   render() {
     return (
+      
       <View style={styles.container}>
-        <Text>
-Open up App.js to start working on your app!
+        <Text style={{ fontSize: 20 }}>
+            Hello
         </Text>
-        <Text>
-Changes you make will automatically reload.
-        </Text>
-        <Text>
-Shake your phone to open the developer menu.
-        </Text>
+        <Button
+          title="Click"
+          onPress={() => Alert.alert(
+          'HeadTitle',
+         'body',
+         [
+          { text: 'Ask me later', onPress: () => console.warn('Ask me later pressed') },
+          { text: 'Cancel', onPress: () => console.warn('Cancel Pressed'), style: 'cancel' },
+          { text: 'OK', onPress: () => console.warn('OK Pressed') },
+        ],
+        { cancelable: false },
+         )}
+        />
       </View>
+
+
     );
   }
 }
+let dd;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -25,4 +37,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});
+
+FCM.requestPermissions().then(() => console.log('granted')).catch(() => console.log('notification permission rejected'));
+
+FCM.getFCMToken().then((token) => {
+  console.log(token);
+  // store fcm token in your server
+});
+
+FCM.on(FCMEvent.Notification, async (notif) => {
+  console.warn(notif);
+  if (notif.opened_from_tray) {
+    console.warn(notif.notification.body);
+  }
 });
